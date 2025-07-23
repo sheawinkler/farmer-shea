54c4d1712b7c5677b871f34669f6891603ce8000
sheawinkler <sheawinkler@gmail.com>
2025-07-23 10:55:40 -0600

    feat: Sprint 3 - Advanced Strategy Implementation (#8)
    * feat(base): add uniswap v3 factory client
    
    * feat(base): implement functional yield farming strategy
    
    * feat(main): update main to use new executor and oracle

--- farmer_shea/base/base.go
+++ farmer_shea/base/base.go
@@ -1,7 +1,16 @@
 package base
 
 import (
+	"context"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/sheawinkler/farmer-shea/base/uniswapv3factory"
+)
+
+const (
+	UniswapV3FactoryAddress = "0x33128a8fC17869897dcE68Ed026d694621f6FDfD"
 )
 
 // Client is a client for interacting with the Base blockchain.	ype Client struct {
@@ -15,4 +24,20 @@ func NewClient(rpcURL string) (*Client, error) {
 		return nil, err
 	}
 	return &Client{client: c}, nil
+}
+
+// GetUniswapV3PoolAddress returns the address of a Uniswap V3 pool.
+func (c *Client) GetUniswapV3PoolAddress(tokenA, tokenB common.Address, fee *big.Int) (common.Address, error) {
+	factoryAddress := common.HexToAddress(UniswapV3FactoryAddress)
+	factory, err := uniswapv3factory.NewUniswapv3factory(factoryAddress, c.client)
+	if err != nil {
+		return common.Address{}, err
+	}
+
+	poolAddress, err := factory.GetPool(nil, tokenA, tokenB, fee)
+	if err != nil {
+		return common.Address{}, err
+	}
+
+	return poolAddress, nil
 }
