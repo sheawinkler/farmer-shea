85a318383ef9ac8cacd91fc5e8e1bc0a763b21a2
Shea Winkler <sheawinkler@gmail.com>
2025-07-23 15:43:01 -0600

    feat(hyperliquid): add GetKlines function

--- farmer_shea/hyperliquid/hyperliquid.go
+++ farmer_shea/hyperliquid/hyperliquid.go
@@ -1,8 +1,11 @@
 package hyperliquid
 
 import (
+	"fmt"
+
 	"github.com/sonirico/go-hyperliquid/pkg/client"
 	"github.com/sonirico/go-hyperliquid/pkg/exchange"
+	"github.com/sonirico/go-hyperliquid/pkg/ws"
 )
 
 // Client is a client for interacting with the Hyperliquid API.	ype Client struct {
@@ -22,4 +25,13 @@ func NewClient() (*Client, error) {
 func (c *Client) DepositToVault(amount string, vaultAddress string) error {
 	_, err := c.client.Exchange.VaultDeposit(amount, vaultAddress)
 	return err
+}
+
+// GetKlines fetches historical klines for a given symbol and interval.
+func (c *Client) GetKlines(symbol string, interval string, limit int) ([]ws.Kline, error) {
+	// The go-hyperliquid library's client.Exchange does not directly expose a klines endpoint.
+	// This would typically require direct HTTP requests to the Hyperliquid API or extending the SDK.
+	// For now, we'll return a dummy response.
+	return []ws.Kline{},
+		fmt.Errorf("GetKlines not yet implemented for Hyperliquid client")
 }
