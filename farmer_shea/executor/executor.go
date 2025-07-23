cb1d0354963def16698b72ecfee5b0bf0b811905
Shea Winkler <sheawinkler@gmail.com>
2025-07-23 10:41:16 -0600

    feat(executor): add retry logic

--- farmer_shea/executor/executor.go
+++ farmer_shea/executor/executor.go
@@ -1,6 +1,8 @@
 package executor
 
 import (
+	"time"
+
 	"github.com/sheawinkler/farmer-shea/strategy"
 	"github.com/sheawinkler/farmer-shea/wallet"
 )
@@ -17,5 +19,13 @@ func NewSimpleExecutor() Executor {
 type simpleExecutor struct{}
 
 func (e *simpleExecutor) Execute(strategy strategy.Strategy, wallet wallet.Wallet) error {
-	return strategy.Execute(wallet)
+	var err error
+	for i := 0; i < 3; i++ {
+		err = strategy.Execute(wallet)
+		if err == nil {
+			return nil
+		}
+		time.Sleep(2 * time.Second)
+	}
+	return err
 }
