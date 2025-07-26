b732bc987ba72147faeb3bfed3691f6c40c2b8db
Shea Winkler <sheawinkler@gmail.com>
2025-07-26 04:00:39 -0600

    feat: Enhance Solana client with ATA and program account functions

--- farmer_shea/solana/solana.go
+++ farmer_shea/solana/solana.go
@@ -3,7 +3,10 @@ package solana
 import (
 	"context"
 
+	"github.com/gagliardetto/solana-go"
+	"github.com/gagliardetto/solana-go/programs/associated-token-account"
 	"github.com/gagliardetto/solana-go/rpc"
+	"github.com/sheawinkler/farmer-shea/wallet"
 )
 
 // Client is a Solana client.
@@ -20,4 +23,48 @@ func NewClient(rpcEndpoint string) (*Client, error) {
 // GetLatestBlockHeight gets the latest block height of the Solana blockchain.
 func (c *Client) GetLatestBlockHeight() (uint64, error) {
 	return c.GetSlot(context.Background(), rpc.CommitmentFinalized)
+}
+
+// GetOrCreateAssociatedTokenAccount gets or creates an associated token account for the given wallet and mint.
+func (c *Client) GetOrCreateAssociatedTokenAccount(w wallet.Wallet, mint solana.PublicKey) (solana.PublicKey, error) {
+	ata, _, err := solana.FindAssociatedTokenAddress(w.PublicKey(), mint)
+	if err != nil {
+		return solana.PublicKey{}, err
+	}
+
+	_, err = c.GetAccountInfo(context.Background(), ata)
+	if err == nil {
+		return ata, nil // Account already exists
+	}
+
+	ix, err := associated_token_account.NewCreateInstruction(w.PublicKey(), w.PublicKey(), mint).Validate()
+	if err != nil {
+		return solana.PublicKey{}, err
+	}
+
+	blockhash, err := c.GetLatestBlockhash(context.Background(), rpc.CommitmentFinalized)
+	if err != nil {
+		return solana.PublicKey{}, err
+	}
+
+	tx, err := solana.NewTransaction([]solana.Instruction{ix}, blockhash.Value.Blockhash, w.PublicKey())
+	if err != nil {
+		return solana.PublicKey{}, err
+	}
+
+	if err := w.SignTransaction(tx); err != nil {
+		return solana.PublicKey{}, err
+	}
+
+	sig, err := c.SendTransaction(context.Background(), tx)
+	if err != nil {
+		return solana.PublicKey{}, err
+	}
+
+	return ata, c.ConfirmTransaction(context.Background(), sig, rpc.CommitmentFinalized)
+}
+
+// GetProgramAccounts gets all accounts owned by a program.
+func (c *Client) GetProgramAccounts(programID string) ([]*rpc.GetProgramAccountsResult, error) {
+	return c.GetProgramAccounts(context.Background(), solana.MustPublicKeyFromBase58(programID), nil)
 }
