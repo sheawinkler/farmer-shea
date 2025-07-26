46d5045101e605b476ca058e257f90a5402a6877
Shea Winkler <sheawinkler@gmail.com>
2025-07-26 04:14:44 -0600

    feat: Add standard deviation calculation to indicators

--- farmer_shea/util/indicators.go
+++ farmer_shea/util/indicators.go
@@ -1,6 +1,8 @@
 package util
 
 import (
+	"math"
+
 	"github.com/sonirico/go-hyperliquid"
 )
 
@@ -15,4 +17,20 @@ func CalculateSMA(klines []hyperliquid.Kline, period int) float64 {
 		sum += klines[i].Close
 	}
 	return sum / float64(period)
+}
+
+// CalculateStdDev calculates the standard deviation of a series of klines.
+func CalculateStdDev(klines []hyperliquid.Kline, period int) float64 {
+	if len(klines) < period {
+		return 0.0
+	}
+
+	mean := CalculateSMA(klines, period)
+
+	sum := 0.0
+	for i := len(klines) - period; i < len(klines); i++ {
+		sum += math.Pow(klines[i].Close-mean, 2)
+	}
+
+	return math.Sqrt(sum / float64(period))
 }
