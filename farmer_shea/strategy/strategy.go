39372657a40048a9514f88eb3f111a914a560661
Shea Winkler <sheawinkler@gmail.com>
2025-07-26 03:50:34 -0600

    feat: Update Strategy interface to include private key

--- farmer_shea/strategy/strategy.go
+++ farmer_shea/strategy/strategy.go
@@ -1,11 +1,13 @@
 package strategy
 
 import (
+	"crypto/ecdsa"
+
 	"github.com/sheawinkler/farmer-shea/wallet"
 )
 
 // Strategy defines the interface for all trading strategies.
 type Strategy interface {
-	Execute(w wallet.Wallet) error
+	Execute(w wallet.Wallet, privateKey *ecdsa.PrivateKey) error
 	Name() string
 }
