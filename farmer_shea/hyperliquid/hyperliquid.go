2429b0e4619f724c0e77a87706704afb1491ab25
Shea Winkler <sheawinkler@gmail.com>
2025-07-26 04:12:59 -0600

    feat: Add GetVaultDetails function to Hyperliquid client

--- farmer_shea/hyperliquid/hyperliquid.go
+++ farmer_shea/hyperliquid/hyperliquid.go
@@ -1,6 +1,7 @@
 package hyperliquid
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -48,4 +49,41 @@ func (c *Client) GetKlines(symbol string, interval string, limit int) ([]hyperli
 	}
 
 	return klines, nil
+}
+
+// VaultDetails represents the details of a Hyperliquid vault.
+type VaultDetails struct {
+	Asset  string  `json:"asset"`
+	Apy    float64 `json:"apy"`
+	Tvl    float64 `json:"tvl"`
+}
+
+// GetVaultDetails fetches the details for a given vault address.
+func (c *Client) GetVaultDetails(vaultAddress string) (*VaultDetails, error) {
+	url := "https://api.hyperliquid.xyz/info"
+	data := []byte(fmt.Sprintf(`{"type": "vaultDetails", "vaultAddress": "%s"}`, vaultAddress))
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var vaultDetails VaultDetails
+	if err := json.Unmarshal(body, &vaultDetails); err != nil {
+		return nil, err
+	}
+
+	return &vaultDetails, nil
 }
