f6e1dc6f68a4a7ea4a471f987dabefb6dbe2f148
Shea Winkler <sheawinkler@gmail.com>
2025-07-26 03:51:00 -0600

    feat: Update Hyperliquid strategy to match new interface

--- farmer_shea/strategy/hyperliquid.go
+++ farmer_shea/strategy/hyperliquid.go
@@ -1,6 +1,7 @@
 package strategy
 
 import (
+	"crypto/ecdsa"
 	"fmt"
 
 	"github.com/sheawinkler/farmer-shea/hyperliquid"
@@ -27,7 +28,7 @@ func (s *simpleVaultDepositStrategy) Name() string {
 	return "SimpleVaultDeposit"
 }
 
-func (s *simpleVaultDepositStrategy) Execute(w wallet.Wallet) error {
+func (s *simpleVaultDepositStrategy) Execute(w wallet.Wallet, privateKey *ecdsa.PrivateKey) error {
 	fmt.Printf("Executing simple vault deposit strategy on Hyperliquid with amount %s to vault %s...\n", s.amount, s.vaultAddress)
 	return s.hyperliquidClient.DepositToVault(s.amount, s.vaultAddress)
 }
